cd6d965005fc31f50c2f9130aafa657fb1ebd583
oforiik <kevinagyei2017@gmail.com>
2025-07-02 14:13:33 +0000

    issue commit

--- frontend-clean/tailwind.config.js
+++ frontend-clean/tailwind.config.js
@@ -1,17 +1,11 @@
-// tailwind.config.js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#4361ee',
-        secondary: '#3f37c9',
-        accent: '#4895ef',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
